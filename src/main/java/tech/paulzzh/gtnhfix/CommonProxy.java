b792348f614a05e3241983ae142cc2619e7a78c7
paulzzh <28055560+paulzzh@users.noreply.github.com>
2023-02-22 15:17:46 +0800

    clean

--- src/main/java/tech/paulzzh/gtnhfix/CommonProxy.java
+++ src/main/java/tech/paulzzh/gtnhfix/CommonProxy.java
@@ -1,6 +1,6 @@
 package tech.paulzzh.gtnhfix;
 
-import cpw.mods.fml.common.event.*;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 public class CommonProxy {
 
@@ -11,36 +11,5 @@ public class CommonProxy {
 
         GTNHFix.info(Config.greeting);
         GTNHFix.info("I am " + Tags.MODNAME + " at version " + Tags.VERSION + " and group name " + Tags.GROUPNAME);
-    }
-
-    // load "Do your mod setup. Build whatever data structures you care about. Register recipes."
-    public void init(FMLInitializationEvent event) {
-
-    }
-
-    // postInit "Handle interaction with other mods, complete your setup based on this."
-    public void postInit(FMLPostInitializationEvent event) {
-
-    }
-
-    public void serverAboutToStart(FMLServerAboutToStartEvent event) {
-
-    }
-
-    // register server commands in this event handler
-    public void serverStarting(FMLServerStartingEvent event) {
-
-    }
-
-    public void serverStarted(FMLServerStartedEvent event) {
-
-    }
-
-    public void serverStopping(FMLServerStoppingEvent event) {
-
-    }
-
-    public void serverStopped(FMLServerStoppedEvent event) {
-
     }
 }
--- src/main/java/tech/paulzzh/gtnhfix/GTNHFix.java
+++ src/main/java/tech/paulzzh/gtnhfix/GTNHFix.java
@@ -2,7 +2,7 @@ package tech.paulzzh.gtnhfix;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
-import cpw.mods.fml.common.event.*;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -13,20 +13,8 @@ public class GTNHFix {
     public static CommonProxy proxy;
     private static Logger LOG = LogManager.getLogger(Tags.MODID);
 
-    public static void debug(String message) {
-        LOG.debug(message);
-    }
-
     public static void info(String message) {
         LOG.info(message);
-    }
-
-    public static void warn(String message) {
-        LOG.warn(message);
-    }
-
-    public static void error(String message) {
-        LOG.error(message);
     }
 
     @Mod.EventHandler
@@ -34,43 +22,5 @@ public class GTNHFix {
     // etc, and register them with the GameRegistry."
     public void preInit(FMLPreInitializationEvent event) {
         proxy.preInit(event);
-    }
-
-    @Mod.EventHandler
-    // load "Do your mod setup. Build whatever data structures you care about. Register recipes."
-    public void init(FMLInitializationEvent event) {
-        proxy.init(event);
-    }
-
-    @Mod.EventHandler
-    // postInit "Handle interaction with other mods, complete your setup based on this."
-    public void postInit(FMLPostInitializationEvent event) {
-        proxy.postInit(event);
-    }
-
-    @Mod.EventHandler
-    public void serverAboutToStart(FMLServerAboutToStartEvent event) {
-        proxy.serverAboutToStart(event);
-    }
-
-    @Mod.EventHandler
-    // register server commands in this event handler
-    public void serverStarting(FMLServerStartingEvent event) {
-        proxy.serverStarting(event);
-    }
-
-    @Mod.EventHandler
-    public void serverStarted(FMLServerStartedEvent event) {
-        proxy.serverStarted(event);
-    }
-
-    @Mod.EventHandler
-    public void serverStopping(FMLServerStoppingEvent event) {
-        proxy.serverStopping(event);
-    }
-
-    @Mod.EventHandler
-    public void serverStopped(FMLServerStoppedEvent event) {
-        proxy.serverStopped(event);
     }
 }

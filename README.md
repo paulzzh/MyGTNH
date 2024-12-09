5417a28d74ce91bfbf0a7b2e3849f0e7d5592fef
paulzzh <28055560+paulzzh@users.noreply.github.com>
2024-12-10 05:37:27 +0800

    cape download cache

--- README.md
+++ README.md
@@ -14,6 +14,8 @@ Any changes can be disabled in `config/mygtnh.cfg`
  - gt_pollute: Used to remove Pollution rendering on client.
  - tank_debuff: Used to remove GT SuperTank debuff.
  - multi_notify: Used to notify multiblock maintenance.
+ - multi_notify_url: multiblock maintenance custom http callback.
+ - gt_cape_url: custom capes. see [CustomSkinAPI](https://github.com/xfl03/CustomSkinLoaderAPI/blob/master/CustomSkinAPI/CustomSkinAPI-zh_CN.md).
  - bop_trail/gs_cape: These mods make network requests when starting the game. Disabling them can improve startup speed under restricted network.
  - gc_cape/gtpp_cape/gt_cape/de_con/de_pic/bot_con/aroma_cape/journey_stat/mc_stat
  - These mods make network requests when starting the game(But in non-blocking threads).
--- src/main/java/com/paulzzh/mygtnh/ClientUtils.java
+++ src/main/java/com/paulzzh/mygtnh/ClientUtils.java
@@ -3,15 +3,16 @@ package com.paulzzh.mygtnh;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.IImageBuffer;
 import net.minecraft.client.renderer.ThreadDownloadImageData;
-import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.util.ResourceLocation;
 
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.Date;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.LinkedHashMap;
 
 import static com.paulzzh.mygtnh.Utils.GSON;
@@ -40,20 +41,18 @@ public class ClientUtils {
                 profile_connection.setReadTimeout(10000);
                 InputStream profile_stream = profile_connection.getInputStream();
                 CustomSkinAPIProfile profile = GSON.fromJson(new InputStreamReader(profile_stream), CustomSkinAPIProfile.class);
-                String cape_url = null;
-                if (profile.cape != null && !profile.cape.isEmpty()) {
-                    cape_url = url_base + "textures/" + profile.cape;
-                } else if (profile.textures.get("cape") != null && !profile.textures.get("cape").isEmpty()) {
-                    cape_url = url_base + "textures/" + profile.textures.get("cape");
+                String cape_hash = profile.cape;
+                if (cape_hash == null || cape_hash.isEmpty()) {
+                    cape_hash = profile.textures.get("cape");
                 }
-                if (cape_url == null) {
+                if (cape_hash == null || cape_hash.isEmpty()) {
                     return;
                 }
-                ResourceLocation resourceLocation = new ResourceLocation(
-                    String.format("mygtnh/capes/%s-%s.png", username, new Date().getTime())
-                );
-                TextureManager textureManager = Minecraft.getMinecraft().getTextureManager();
-                ThreadDownloadImageData threadDownloadImageData = new ThreadDownloadImageData(null, cape_url, null, new IImageBuffer() {
+                String cape_url = url_base + "textures/" + cape_hash;
+                File cache = Paths.get("capes_cache/" + cape_hash).toFile();
+                Files.createDirectories(Paths.get("capes_cache/"));
+                ResourceLocation rL = new ResourceLocation(String.format("mygtnh/capes/%s", cape_hash));
+                ThreadDownloadImageData dL = new ThreadDownloadImageData(cache, cape_url, null, new IImageBuffer() {
                     @Override
                     public BufferedImage parseUserSkin(BufferedImage p_78432_1_) {
                         return p_78432_1_;
@@ -61,10 +60,10 @@ public class ClientUtils {
 
                     @Override
                     public void func_152634_a() {
-                        MyGTNH.CAPE_CACHE.put(username, resourceLocation);
+                        MyGTNH.CAPE_CACHE.put(username, rL);
                     }
                 });
-                textureManager.loadTexture(resourceLocation, threadDownloadImageData);
+                Minecraft.getMinecraft().getTextureManager().loadTexture(rL, dL);
             } catch (Exception e) {
                 e.printStackTrace();
             }

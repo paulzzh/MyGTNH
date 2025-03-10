f3ac37cc55ea3735a713ac9df63e863b383ecb5b
paulzzh <28055560+paulzzh@users.noreply.github.com>
2025-03-10 13:54:12 +0800

    Revert "remove @Redirect"
    This reverts commit afc7c64b6a3ec843276be936234ad6f964942e18.

--- src/main/java/com/paulzzh/mygtnh/mixins/late/gregtech/RecipeLoader2Mixin.java
+++ src/main/java/com/paulzzh/mygtnh/mixins/late/gregtech/RecipeLoader2Mixin.java
@@ -1,13 +1,13 @@
 package com.paulzzh.mygtnh.mixins.late.gregtech;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import goodgenerator.loader.RecipeLoader2;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(value = RecipeLoader2.class)
 public class RecipeLoader2Mixin {
-    @ModifyExpressionValue(method = "RecipeLoad()V", at = @At(value = "CONSTANT", args = "intValue=114514"), remap = false)
+    @ModifyConstant(method = "RecipeLoad()V", constant = @Constant(intValue = 114514), remap = false)
     private static int injected1(int value) {
         return 111111;
     }

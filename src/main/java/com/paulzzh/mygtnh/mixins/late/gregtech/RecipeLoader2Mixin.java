afc7c64b6a3ec843276be936234ad6f964942e18
paulzzh <28055560+paulzzh@users.noreply.github.com>
2025-03-10 13:40:49 +0800

    remove @Redirect

--- src/main/java/com/paulzzh/mygtnh/mixins/late/gregtech/RecipeLoader2Mixin.java
+++ src/main/java/com/paulzzh/mygtnh/mixins/late/gregtech/RecipeLoader2Mixin.java
@@ -1,13 +1,13 @@
 package com.paulzzh.mygtnh.mixins.late.gregtech;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import goodgenerator.loader.RecipeLoader2;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(value = RecipeLoader2.class)
 public class RecipeLoader2Mixin {
-    @ModifyConstant(method = "RecipeLoad()V", constant = @Constant(intValue = 114514), remap = false)
+    @ModifyExpressionValue(method = "RecipeLoad()V", at = @At(value = "CONSTANT", args = "intValue=114514"), remap = false)
     private static int injected1(int value) {
         return 111111;
     }
